7995884ac3ed27d6c203406962e35eb813dfed89
David Wolfe <davidgameswolfe@gmail.com>
2013-10-30 15:54:00 -0300

    Imports happen from root of sheepdog tables app

--- sheepdog_tables/__init__.py
+++ sheepdog_tables/__init__.py
@@ -1,1 +1,17 @@
-__version__ = '1.0.4'
+__version__ = '1.0.5'
+
+try:
+    from django.conf import settings
+    getattr(settings, 'dummy_attr', 'dummy_value')
+    _LOAD_PACKAGES = True
+except:
+    # Just running sdist, we think
+    _LOAD_PACKAGES = False
+
+if _LOAD_PACKAGES:
+    from mixins import (TablesMixin, EditTablesMixin, FilteredListView,
+                        SortFilterMixin, CSVTableMixin)
+    from column import ColumnURL, Column, DictColumn, FieldColumn
+    from filters import (SortChoice, SortChoiceField, SortChoiceFilter,
+                         BaseSortableFilterSet)
+    from table import Table, EditTable
